6e9ab85b485e25a836c9c665a421b2d6aec612ef
Eduardo de Almeida Fernandes <eduardoa.fernandes@hotmail.com>
2023-07-16 15:25:17 -0300

    feat (req06): Criado rota GET /user/:id e suas validações

--- src/controllers/User.js
+++ src/controllers/User.js
@@ -5,6 +5,16 @@ const findAll = async (req, res) => {
     return res.status(200).json(users);
 };
 
+const findById = async (req, res) => {
+  const { id } = req.params;
+  const user = await UserService.getUserById(id);
+  if (!user) {
+    return res.status(404).json({ message: 'User does not exist' });
+  }
+  return res.status(200).json(user);
+};
+
 module.exports = {
   findAll,
+  findById,
 };
--- src/routes/user.js
+++ src/routes/user.js
@@ -3,5 +3,6 @@ const { userController } = require('../controllers');
 const validateJWT = require('../middlewares/validateJWT');
 
 route.get('/user', validateJWT, userController.findAll);
+route.get('/user/:id', validateJWT, userController.findById);
 
 module.exports = route;
